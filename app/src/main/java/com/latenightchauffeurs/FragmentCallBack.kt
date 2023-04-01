0f1de7ecf09d951aad23ba3bd3e187df0b3cc2ad
356400214 <siru>
2023-04-01 21:12:06 +0530

    DBH implementation

---
+++ app/src/main/java/com/latenightchauffeurs/FragmentCallBack.kt
@@ -0,0 +1,9 @@
+package com.latenightchauffeurs
+
+/**
+ * Create by Sirumalayil on 01-04-2023.
+ */
+interface FragmentCallBack {
+
+    fun onResult(param1: Any?, param2: Any?= null, param3: Any? = null)
+}
