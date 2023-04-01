5d801574b98869bee27ecbcced9963c5efa5c362
356400214 <siru>
2023-04-01 16:55:19 +0530

    DBH date picker and timer picker implemntation

--- app/src/main/java/com/latenightchauffeurs/fragment/DriverByTheHourFragment.kt
+++ app/src/main/java/com/latenightchauffeurs/fragment/DriverByTheHourFragment.kt
@@ -1,15 +1,21 @@
 package com.latenightchauffeurs.fragment
 
-import android.content.DialogInterface
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
 import com.google.android.material.datepicker.MaterialDatePicker
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.latenightchauffeurs.databinding.DriverByTheHourLayoutBinding
-import java.util.Objects
+import java.text.SimpleDateFormat
+import java.util.*
 
 /**
  * Create by Sirumalayil on 01-04-2023.
@@ -54,27 +60,81 @@ class DriverByTheHourFragment: Fragment() {
             activity?.finish()
         }
 
-        binding?.cardViewDatePicker?.setOnClickListener {
+        binding?.textDate?.setOnClickListener {
             showDatePickerDialog()
         }
 
-        binding?.cardViewTimePicker?.setOnClickListener {
+        binding?.textTime?.setOnClickListener {
             showTimePickerDialog()
         }
     }
 
     private fun showTimePickerDialog() {
+        // Get the current time in hours and minutes
+        val currentTime = Calendar.getInstance()
+        val currentHour = currentTime.get(Calendar.HOUR_OF_DAY)
+        val currentMinute = currentTime.get(Calendar.MINUTE)
 
+        // Create a MaterialTimePicker.Builder object
+        val timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_12H)
+            .setHour(currentHour)
+            .setMinute(currentMinute)
+            .setTitleText("Select Time")
+            .build()
+
+        // Set the time picker to allow selection of only future times
+        timePicker.addOnPositiveButtonClickListener {
+            val selectedHour = timePicker.hour
+            val selectedMinute = timePicker.minute
+            val calendar = Calendar.getInstance()
+            calendar.set(Calendar.HOUR_OF_DAY, selectedHour)
+            calendar.set(Calendar.MINUTE, selectedMinute)
+
+            // Check if the selected time is in the future
+            if (calendar.timeInMillis > System.currentTimeMillis()) {
+                // Do something with the selected time
+                val selectedTime = "${selectedHour}:${selectedMinute}"
+                binding?.textTime?.setText(selectedTime)
+            } else {
+                Toast.makeText(activity, "Please select a future time", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Show the time picker dialog
+        activity?.supportFragmentManager?.let { timePicker.show(it, "time_picker") }
     }
 
+    @SuppressLint("SimpleDateFormat")
     private fun showDatePickerDialog() {
         val builder = MaterialDatePicker.Builder.datePicker()
+            .setCalendarConstraints(
+                constraintsBuilder.build()
+            )
             .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
 
         val picker = builder.build()
         picker.addOnPositiveButtonClickListener {
+            val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+            calendar.time = Date(it)
 
+            val month = calendar.get(Calendar.MONTH)
+            val year = calendar.get(Calendar.YEAR)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            calendar.set(year,month, day)
+            val format = SimpleDateFormat("MM-dd-yyyy")
+            val strDate: String = format.format(calendar.time)
+            binding?.textDate?.setText(strDate)
         }
         activity?.supportFragmentManager?.let { picker.show(it, "date_picker") }
     }
+
+    /**
+     * Allowing that enables dates from a given point forward.
+     * Defaults to the current moment in device time forward using now(),
+     * but can be set to any point, as UTC milliseconds, using from(long).
+     */
+    private val constraintsBuilder = CalendarConstraints.Builder()
+            .setValidator(DateValidatorPointForward.now())
 }
